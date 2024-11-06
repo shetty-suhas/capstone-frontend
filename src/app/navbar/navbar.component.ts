b9a0fe9cf867bbf8a6b4463f622bc6896f71f654
suhas <shettysuhasc@gmail.com>
2024-11-06 16:36:18 +0530

    navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent { 
+  iconSrcAdd: string = "assets/add-grey.png" 
+  iconSrcEvent: string = "assets/event-grey.png" 
+  iconSrcGuest: string = "assets/guest-grey.png"   
+  iconSrcBudget: string = "assets/grey-budget.png"
+
+  onHoverAdd(isHovered: boolean): void {
+    this.iconSrcAdd = isHovered ? "assets/add-white.png" : "assets/add-grey.png";
+  } 
+
+  onHoverEvent(isHovered: boolean): void {
+    this.iconSrcEvent = isHovered ? "assets/event-white.png" : "assets/event-grey.png";
+  } 
+  onHoverGuest(isHovered: boolean): void {
+    this.iconSrcGuest = isHovered ? "assets/guest-white.png" : "assets/guest-grey.png";
+  } 
+  onHoverBudget(isHovered: boolean): void {
+    this.iconSrcBudget = isHovered ? "assets/budget-white.png" : "assets/budget-grey.png"; 
+  }
+}
